5995b4e90caa618b23caed97cb783cbe8759b8f1
Peter Gille <petergil@idem.me>
2015-11-10 05:52:29 +0100

    Spell fix

--- pid.handson.sh
+++ pid.handson.sh
@@ -7,7 +7,7 @@ function epic_request() {
 	echo "Request:"
 	echo "\tcurl --user $PREFIX:\$PASSWORD --header \"$ACCEPT\" \"$SERVER$1\""
 	echo ""
-	echo "Press any key to send te request"
+	echo "Press any key to send the request"
 	read -n 1 -s
 	echo ""
 	echo "Response:"
@@ -19,7 +19,7 @@ function epic_create() {
 	echo "Request:"
 	echo "\tcurl -s -D - --fail --user $PREFIX:\$PASSWORD --data @$2 --header \"Content-type: application/json\" \"$SERVER$1\" -o /dev/null"
         echo ""
-        echo "Press any key to send te request"
+        echo "Press any key to send the request"
         read -n 1 -s
 	echo ""
         echo "Response:"
@@ -31,7 +31,7 @@ function epic_modify() {
 	echo "Request:"
         echo "\tcurl -s -D - --user $PREFIX:\$PASSWORD --upload-file $2 --header \"Content-type: application/json\" \"$SERVER$1$3\" -o /dev/null"
         echo ""
-        echo "Press any key to send te request"
+        echo "Press any key to send the request"
         read -n 1 -s
 	echo ""
         echo "Response:"
@@ -43,7 +43,7 @@ function epic_delete() {
 	echo "Request:"
         echo "\tcurl -s -D - --user $PREFIX:\$PASSWORD -X DELETE  \"$SERVER$1$2\" -o /dev/null"
         echo ""
-        echo "Press any key to send te request"
+        echo "Press any key to send the request"
         read -n 1 -s
 	echo ""
         echo "Response:"
